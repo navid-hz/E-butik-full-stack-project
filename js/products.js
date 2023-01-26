8bb7a0ad8992284a0cda816b6fb072391e9b76c7
Navid Hosseini <hosseini.zari@gmail.com>
2023-01-26 23:29:13 +0100

    Product page JS (temporary)

---
+++ js/products.js
@@ -0,0 +1,45 @@
+const productsCardsContainer = document.getElementById('products-cards-container');
+
+const renderProducts = async () => {
+    let url = 'http://localhost:5000/api/store';
+    let response = await fetch(url);
+    let products = await response.json();
+    
+    if (response.status !== 200) {
+        throw new Error('Cannot fetch the data');
+    } else {
+        if (products.length === 0) {
+            productsCardsContainer.innerHTML = '<h1>There are no products</h1>';
+        } else {
+            let template = '';
+            products.forEach(products => {
+                template += `
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light"     data-mdb-ripple-color="light">
+                                <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12.jpg"           class="w-100" />
+                                <a href="#!">
+                                    <div class="hover-overlay">
+                                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <a href="" class="text-reset">
+                                    <h5 class="card-title mb-2">${products.title}</h5>
+                                </a>
+                                <a href="" class="text-reset ">
+                                    <p>${products.categoty[0]}</p>
+                                </a>
+                                <h6 class="mb-3 price">${products.prise}</h6>
+                            </div>
+                        </div>
+                    </div>
+                `;
+            });
+        }
+    }
+    productsCardsContainer.innerHTML = template;
+}
+
+window.addEventListener('DOMContentLoaded', () => renderProducts());
